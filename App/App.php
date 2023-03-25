78ca176453ab5b06330100e9efb6239b8e6ca381
MonikaJaraite <monjara@gmail.com>
2023-03-25 22:50:17 +0200

    Clients pries views output buferinima

---
+++ App/App.php
@@ -0,0 +1,40 @@
+<?php
+namespace App;
+
+use App\Controllers\HomeController;
+
+class App {
+
+    public static function process() 
+    {
+
+        $url = explode('/', $_SERVER['REQUEST_URI']);
+        array_shift($url);
+
+        return self::router($url);
+    }
+
+    private static function router(array $url) 
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method == 'GET' && count($url) == 1 && $url[0] === '') {
+            return (new HomeController)->home();
+        }
+        else {
+            return '<h1>404 PAGE NOT FOUND</h1>';
+        }
+    }
+
+    public static function views($tmp) 
+    {
+        $path = __DIR__ . '/../views/';
+
+        require $path . 'top.php';
+
+        require $path . $tmp . '.php';
+
+        require $path . 'bottom.php';
+    }
+
+}
---
+++ App/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controllers;
+use App\App;
+
+class HomeController {
+
+    public function home()
+    {
+        return App::views('home/index');
+    }
+
+}
